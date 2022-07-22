2d1b06397adbe1d868bb868d8091f7c5a7d9ea04
bonbo0811 <abcdef999gp811@gmail.com>
2022-07-22 23:37:04 +0900

    js popper / modal追加

---
+++ Modals/Target-modal.php
@@ -0,0 +1,31 @@
+<!-- Modal -->
+<div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="js-modal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <p class="px-2 me-3 bg-info rounded text-white">Mission</p>
+                <h4 class="modal-title" id="exampleModalLabel">ガタイをよくする！</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mt-1 mb-3">
+                    <label for="" class="mb-2 bg-info rounded text-white">　Target　</label><br>
+                    <p class="ms-3 mb-0">ダンベル200回</p>
+                </div>
+                <div class="mt-1 mb-3">
+                    <label for="" class="mb-2 bg-info rounded text-white">　頻度　</label><br>
+                    <p class="ms-3 mb-0">毎日</p>
+                </div>
+                <div class="mt-1 mb-3">
+                    <label for="" class="mb-2 bg-info rounded text-white">　コメント　</label><br>
+                    <p class="ms-3 mb-0">2キロのダンベルを毎日左右100×2セット</p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href=" " class="btn btn-primary">完了</a>
+                <a href=" " class="btn  btn-outline-success">編集</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal -->
